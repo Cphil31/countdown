19fc9c4983a1e3e0f726d81654713d71fbe3103c
Mooncode29 <irene.ravelonarivo@gmail.com>
2016-10-14 15:29:53 +0200

    double digit ok

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 console.log('hello world')
-var timer;
+var timer = 120;
 var interval;
 
-	function reset(){
-			timer = 5;
-		interval = setInterval(function (){
-			timer--;
-		console.log(interval);
-			$('#countdown_sec').html(timer);
-			if(timer===0){
+
+function reset(){
+	interval = setInterval(function (){
+		timer--;
+		var minute = parseInt(timer/60, 10);
+		console.log(minute);
+		var seconde = parseInt(timer - minute*60, 10);
+		if(minute < 10){
+			minute = '0'+ minute; 
+		}
+		if(seconde < 10){
+			seconde = '0' + seconde;
+		}
+		
+		
+		
+				
+	
+		$("#minute").html(minute);
+		$("#seconde").html(seconde);
+		
+		if(timer===0){
 			clearInterval(interval);
-						}
+		}
+		
 	}, 1000);
-	}
+}
 
-$("#reset").click(function(){
-	reset();
 
-});
 reset();
+
+// function twoDigit(number) {
+//   var twodigit = number >= 10 ? number : "0"+number.toString();
+//   return twodigit;
+// }
